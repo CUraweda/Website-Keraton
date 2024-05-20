a615fc84d29a109521f97f50eade02dfe9441a59
Alipa Gian Rafid <taiee8288@gmail.com>
2024-05-20 09:18:31 +0700

    Slider News

--- quasar-project/src/pages/SejarahKeraton.vue
+++ quasar-project/src/pages/SejarahKeraton.vue
@@ -6,76 +6,77 @@
     rel="stylesheet">
   <div id="app">
     <div class="textAll">
-    <nav>
-      <navbar border is-checkout-page="true"/>
-    </nav>
+      <nav>
+        <navbar border is-checkout-page="true" />
+      </nav>
       <div class="cover">
-       <img src="../assets/images/sejarah.jpg" alt="sejarah" class="fotobkg"> 
-       <div class="tulisan">
-        <div class="upper">
-          {{ uppertext }}
-        </div>
-        <div class="upper-2">
-          {{ uppertext2 }}
+        <img src="../assets/images/sejarah.jpg" alt="sejarah" class="fotobkg">
+          <div class="upper">
+            {{ uppertext }}
           </div>
-        </div>
+          <div class="upper-2">
+            {{ uppertext2 }}
+          </div>
       </div>
       <div class="info">
         <div class="info1">
           <p>Kota Cirebon merupakan kota yang memiliki nilai historis yang sangat kental terutama pada masa
             kerajaan dan penyebaran agama Islam di Pulau Jawa.
-  
+
             <br><br>Salah satu peninggalan pada masa
             kerajaan tersebut adalah sebuah bangunan yang hingga kini masih berdiri dengan kokoh di kota
             ini yaitu Rumah Adat Kasepuhan Cirebon.
           </p>
           <img src="../assets/images/macanalikasepuhan.png" alt="">
         </div>
-  
-        <div class="seluruh">
         <div class="info2">
-          <img src="../assets/images/pendopo.png" alt="">
+          <img src="../assets/images/pendopo.png" class="img-keraton2" alt="">
           <div class="info2-text">
-  
-            <p class="between">Pada abad XV (-+ 1430)
-  
-              <br><br>Pangeran Cakrabuwana Putra mahkota Kerajaan Pajajaran membangun Keraton yang kemudian
-              diserahkan kepada putrinya, Ratu Ayu Pakungwati. Maka Keratonnya dinamai Keraton Pakungwati
-              (hingga sekarang dikenal dengan sebutan Dalem Agung Pakungwati)
-            </p>
+            <div>
+              <p class="between">Pada abad XV (-+ 1430)</p>
+              <p>
+                Pangeran Cakrabuwana Putra mahkota Kerajaan Pajajaran membangun Keraton yang kemudian
+                diserahkan kepada putrinya, Ratu Ayu Pakungwati. Maka Keratonnya dinamai Keraton Pakungwati
+                (hingga sekarang dikenal dengan sebutan Dalem Agung Pakungwati)
+              </p>
+            </div>
+            <img src="../assets/images/timeline.png" alt="" class="time">
           </div>
-          <img src="../assets/images/timeline.png" alt="" class="time">
-  
         </div>
-        
+
         <div class="info3">
           <img src="../assets/images/timeline1.png" alt="" class="time1">
           <div class="info3-text">
-            <p class="jarak">Pada abad XVI</p>
-            <p class="jarak1"> <br>Sunan Gunung Jati wafat. Kemudian Pangeran Emas Mochammad Arifin, cicit dari Sunan
-              Gunung Jati,
-              bertahta menggantikannya. Kemudian pada tahun Candra Sangkala Tunggal Tata Gunaning Wong atau
-              1451 Saka yaitu tahun 1529 Masehi beliau mendirikan Keraton baru sebelah barat daya Dalem Agung
-              Pakungwati.
-              Dan beliaupun bergelar Panembahan Pakungwati I.
-            </p>
+            <div>
+              <p class="between2">Pada abad XVI</p>
+              <p>Sunan Gunung Jati wafat. Kemudian Pangeran Emas Mochammad Arifin, cicit dari Sunan
+                Gunung Jati,
+                bertahta menggantikannya. Kemudian pada tahun Candra Sangkala Tunggal Tata Gunaning Wong atau
+                1451 Saka yaitu tahun 1529 Masehi beliau mendirikan Keraton baru sebelah barat daya Dalem Agung
+                Pakungwati.
+                Dan beliaupun bergelar Panembahan Pakungwati I.
+              </p>
+            </div>
           </div>
-          <img src="../assets/images/k1.png" alt="" class="jarak2">
+          <img src="../assets/images/k1.png" alt="" class="img-keraton3">
         </div>
         <div class="info4">
-          <img src="../assets/images/m1.png" alt="" class="distance">
+          <img src="../assets/images/m1.png" alt="" class="img-keraton4">
           <div class="info4-text">
-            <p class="jarak3">Pada -+ 1549 M </p>
-            <p><br>Masjid Agung Sang Cipta Rasa kebakaran. Ratu Ayu Pakungwati yang sudah tua itu turut memadamkan api.
+          <div>
+            <p class="between4">Pada -+ 1549 M </p>
+            <p>Masjid Agung Sang Cipta Rasa kebakaran. Ratu Ayu Pakungwati yang sudah tua itu turut memadamkan api.
               Api dapat dipadamkan, namun Ratu Ayu Pakungwati kemudian wafat. Semenjak itu, nama atau sebutan Pakungwati
               dimuliakan dan diabadikan oleh nasab Sunan Gunung Jati.</p>
-  
-          </div><img src="../assets/images/timeline2.png" alt="">
+          </div>
+          </div>
+          <img src="../assets/images/timeline2.png" alt="" class="time">
         </div>
         <div class="info5">
-          <img src="../assets/images/timeline3.png" alt="" class="time2">
-          <div class="">
-            <p class="jarak4">Pada -+ 1679 M</p>
+          <img src="../assets/images/timeline3.png" alt="" class="time5">
+          <div class="info5-text">
+          <div>
+            <p class="between5">Pada -+ 1679 M</p>
             <p>
               Didirikan Keraton Kanoman oleh Sultan Anom I (Sultan Badridin). Maka semenjak itu, Keraton Pakungwati
               disebut
@@ -86,21 +87,20 @@
               Kasepuhan membujur dari utara ke Selatan atau menghadap ke utara, karena keraton-keraton di Jawa semuanya
               menghadap
               ke utara, artinya menghadap magnet dunia, artinya falsafahnya sang raja mengharapkan kekuatan.</p>
-  
-          </div><img src="../assets/images/n1.png" alt="" class="foto">
-        </div>
+          </div>
+          </div>
+          <img src="../assets/images/n1.png" alt="" class="img-keraton5">
         </div>
       </div>
-      
+
       <div class="footer">
-  
         <div class="logo">
           <img src="../assets/images/logo_keraton.png" alt="">
           <p>KERATON <br>KASEPUHAN <br>CIREBON</p>
         </div>
         <div class="footer-col">
           <div class="footer-col-1">
-            <h3>Quick Links</h3>
+            <h4>Quick Links</h4>
             <ul>
               <li><a href="#">Beranda</a></li>
               <li><a href="#">Sejarah</a></li>
@@ -109,7 +109,7 @@
             </ul>
           </div>
           <div class="footer-col-2">
-            <h3>Socials</h3>
+            <h4>Socials</h4>
             <ul>
               <li><a href="#">Whatsapp</a></li>
               <li><a href="#">Facebook</a></li>
@@ -118,7 +118,7 @@
             </ul>
           </div>
           <div class="footer-col-3">
-            <h3>Company</h3>
+            <h4>Company</h4>
             <ul>
               <li><a href="#">About us</a></li>
               <li><a href="#">Partners</a></li>
@@ -126,10 +126,8 @@
             </ul>
           </div>
           <div class="footer-col-4">
-            <h3>Subscribes your email for updates!</h3>
+            <h4>Subscribes your email for updates!</h4>
             <button class="button">Enter your email</button>
-  
-  
           </div>
         </div>
         <div class="adress">
@@ -139,461 +137,351 @@
         </div>
         <div class="lower">
           <div class="inlower">
-            @2024 Keraton Kasepuhan Cirebon
+            <p>
+              @2024 Keraton Kasepuhan Cirebon
+            </p>
           </div>
-          <div class="susun">
+          <div class="footer-kanan">
+            <div class="susun">
             <p>In collaboration</p>
-  
             <div class="collab">
               <img src="../assets/images/logo_keraton.png" alt="" class="foto1">
               <img src="../assets/images/1 931.svg" alt="" class="foto2">
               <img src="../assets/images/telkom.svg" alt="" class="foto3">
               <img src="../assets/images/bjb.svg" alt="" class="foto4">
-  
             </div>
+          </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  </template>
-  
-  <script setup>
-  // import navbar from "quasar-project/src/components/NavBar.vue";
-  
-  </script>
-  
-  <script>
-  import navbar from '../components/NavBar.vue'
-  export default {
-    data() {
-      return {
-        uppertext: 'Warisan Leluhur Cirebon',
-        uppertext2: 'Mengenal Lebih Dekat dengan Sejarah Keraton',
-        
-    
-      };
-      
-    },
-    components: {
-      navbar
-    }
-  };
-  
-  </script>
-  
-  <style scoped>
-  
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    overflow: hidden;
-  }
-  
-  body{
-      width: 1280px;
-    }
-  
-    .cover img{
-      width: 1485px;
-      height: 720px;
-      margin-left: -100px;
-    }
-  
-  
-  /* 1024 */
-  @media only screen and (min-width: 1024px) {
-  /* Penyesuaian untuk ukuran desktop 1024px */
-  
-  .seluruh{
-    margin-left: 30%;
-  }
-  .cover{
-    margin-left: 10%;
-    width: 1200px;
-  }
-  .tulisan{
-    margin-top: -6%;
-    margin-left: 8%;
-  }
-  .info1{
-    margin-top: 5%;
-    margin-right: -350px;
-  }
-  
-  }
-  
-  @media only screen and (min-width: 1440px) {
-  /* Penyesuaian untuk ukuran desktop 1440px */
-  
-  .seluruh{
-    margin-left: 6%;
-  }
-  .cover{
-    margin-left: 6.5%;
-    width: 1290px;
-  }
-  .tulisan{
-    margin-top: 2%;
-    margin-left: -8%;
-  }
-  .info1{
-    margin-top: 5%;
-    margin-left: -250px;
-  }
-  
-  
-  }
-  
-  
-  .button {
-   width:431px;
-   height: 58px;
-   border-radius: 10px;
-   background-color: #123B32;
-   color: #FFFFFF;
-   text-align: left;
-   padding-left: 22px;
-   font-size: 20px;
-   font-weight: 800;
-   margin-top: 22px;
-   line-height: 28px;
-   font-family: "Raleway";
-   border: none;
-  
-  }
-  .textAll {
-    font-family: "Raleway", sans-serif;
-    font-optical-sizing: auto;
-    font-weight: 400;
-    font-style: normal;
-    color: #212121;  
-  }
-  
-  .fotobkg {
-    width:100%;
-    height: auto;
-    margin-top: 90px;
+</template>
 
+<script setup>
+// import navbar from "quasar-project/src/components/NavBar.vue";
+
+</script>
+
+<script>
+import navbar from '../components/NavBar.vue'
+export default {
+  data() {
+    return {
+      uppertext: 'Warisan Leluhur Cirebon',
+      uppertext2: 'Mengenal Lebih Dekat dengan Sejarah Keraton',
+
+
+    };
+
+  },
+  components: {
+    navbar
   }
-  .upper{
-    position: absolute;
-    margin-top:-31%;
-    margin-left:50%;
-    transform: translate(-50%, -50%);
-    color: #FAE084;
-    font-size: 2.4vw;
-  
-  }
-  .upper-2 {
-      position:absolute;
-      margin-top: -23.5%;
-      margin-left:50%;
-      transform: translate(-50%, -50%);
-      color: #FFF6E0;
-      font-size: 5vw;
-      font-weight:400;
-      width: 60%;
-      text-align: center;
-  }
-  
-  .info {
-    
-    margin-left: 108px;
-    margin-right: 108px;
-    padding-top: 77px;
-   
-    
-  }
-  .info1 {
-    justify-content: center;
-    display: flex;
-    align-items: center;
-    width:auto;
-  }
-  .info1 p {
-    margin:0;
-    font-size: 30px;
-    margin-right: 59px;
-    font-weight: 400;
-    color: #212121;
-    line-height: 40px;
-    width:522px;
-  }
-  .info1 img {
-    height: 357;
-    width: 483;
-    border-radius: 10px;
-    margin-right: -15px;
-  }
-  .info2 {
-    justify-content: center;
-    display: flex;
-    align-items: center;
-    margin-top: 157px;
-    width:auto;
-    margin-left: 5vw;
-  
-    
-  }
-  
-  .between {
-   
-   padding-top: 15px;
-  }
-  .info2 p {
-    font-size: 30px;
-    font-weight: 400;
-    color:#212121;
-    text-align: right ;
-    width: 500px;
-    margin-bottom: 100px;
-    margin-top:100px;
-    line-height: 40px;
-    
-    
-  }
-  
-  .info2-text {
-    display: flex;
-    flex-direction: row;
-   
-  }
-  .info2 img {
-    height: auto;
-    border-radius: 10px;
-    height: 426px;
-  }
-  .time {
-    position: relative;
-    margin-left: 24px;
-    padding-bottom: 40px;
-    margin-right: 50px;
-  }
-  .time1 {
-    margin-left:-10px;margin-right: 5px;width:40px;    
-  }
-  
-  .info3 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 114px;
-  }
-  
-  .info3-text {
-    font-size: 30px;
-    font-weight: 400;
-    color:#212121;
-    margin-right: 22px;
-    width: 500px;
-    margin-top:48px;
-    line-height: 40px;
-  }
-  .jarak {
-    margin-left: 29px;
-   
-  }
-  .jarak1{
-    margin-bottom: 57px;
-    
-    width:509px;
-  }
-  .jarak2 {
-    padding-left: 90px;
-  
-  }
-  .jarak4 {
-    padding-top: 27px;
-    padding-bottom: 17px;
-    margin-left: 29px;
-  }
-  .jarak3{
-   margin-right: 29px;
-   margin-top:10px;
-  }
-  
-  
-  
-  .info4 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 67px;
-  }
-  .info4 p {
-    font-size: 30px;
-    font-weight: 400;
-    color:#212121;
-    text-align: right ;
-    width: 542px;
-    line-height: 40px;
-  }
-  .distance {
-    margin-right: 100px;
-  }
-  .info4-text {
-    display: flex;
-    flex-direction: column;
-    
-    align-items: center;
-    margin-right: 1px;
-  }
-  
-  .info5 {
-    margin-top: 158px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-left: 30px;
-    margin-right: 108px;
-    
-    
-  }
-  
-  .info5 p {
-  font-size: 30px;
-  font-weight: 400;
-  color: #212121;
+};
+
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  overflow: hidden;
+}
+
+body {
+  width: 1280px;
+}
+
+.fotobkg {
+  width: 100%;
+  height: auto;
+  margin-top: 90px;
+  height: 100%;
+  object-fit: cover;
+}
+
+.button {
+  width: 431px;
+  height: 58px;
+  border-radius: 10px;
+  background-color: #123B32;
+  color: #FFFFFF;
   text-align: left;
-  width: 500px;
-  display: flex;
-  line-height: 40px;
-  }
-  
-  .info5 img {
-  margin-left: 50px;
-  }
-  
-  .foto {
-    width:483px;
-    height: 294px;
-  }
-  .foto1 {
-   width:68px;
-   height: 71px;
-  }
-  .foto2 {
-   width:69px;
-   height: 74px;
-  }
-  .foto3 {
-   width:53px;
-   height: 71px;
-  }
-  .foto4 {
-   width : 100px;
-   height: 49px;
-  }
-  .footer {
-   width: 100%;
-   height: auto;
-   margin-top: 278px;
-   position: relative;
-    margin-bottom: 100px;
-   margin-left: 50px;;
-  } 
-  a {
-   text-decoration: none;
-   color: #212121;
-  }
-  ul {
-   list-style-type: none;
-  }
-  .footer-col {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  gap: 20px;
-  }
-  
-  .footer-col h3 {
-   font-size: 30px;
-   font-weight: 40px;
-   display: inline-block;
-   margin-bottom: 11px;
-   margin-top: 76px;
-  }
-  .footer-col-1 {
-   
-   margin-left: 88px;
-   width: 20vw;
-   
-  }
-  
-  
-  
-  .logo {
+  padding-left: 22px;
   font-size: 20px;
+  font-weight: 800;
+  margin-top: 22px;
+  line-height: 28px;
+  font-family: "Raleway";
+  border: none;
+
+}
+
+.textAll {
+  font-family: "Raleway", sans-serif;
+  font-optical-sizing: auto;
   font-weight: 400;
-  display: flex;
-  flex-direction: row;
-  margin-left: 88px;
-  
-  
-  }
-  
-  .logo img {
-  width: 70px;
-  margin-right: 9px;
-  
-  }
-  
-  .footer-col-2 {
-  margin-left: 60px;
-  }
-  
-  .footer-col-3 {
-  margin-left: 60px;
-  }
-  
-  .footer-col-4 {
-  margin-left: 139px;
-  width: 434px;
-  }
-  
-  .footer-col li {
+  font-style: normal;
   color: #212121;
-  }
-  
-  .footer-col li:not(:last-child) {
-  margin-bottom: 11px;
-  }
-  
-  .adress {
-  width: 184px;
-  height: 84px;
-  font-size: 20px;
-  font-weight: 400;
-  margin-top: 62px;
-  margin-left: 88px;
-  }
-  
-  .lower {
-  
-  height: 142px;
-  margin-left: 88px;
-  display: flex;
-  }
-  
-  .inlower {
-  padding-top: 118px;
+}
+
+.upper {
   position: absolute;
-  }
-  
-  .collab {
+  margin-top: -30vw;
+  margin-left: 50%;
+  transform: translate(-50%, -50%);
+  color: #FAE084;
+  font-size: 2vw;
+  white-space: nowrap;
+}
+
+.upper-2 {
+  position: absolute;
+  margin-top: -24vw;
+  margin-left: 50%;
+  transform: translate(-50%, -50%);
+  color: #FFF6E0;
+  font-size: 3.5vw;
+  font-weight: 400;
+  width: 40vw;
+  text-align: center;
+}
+
+.info {
+  padding-top: 10%;
+  margin: 0 auto;
+  width: 80%;
+}
+
+.info1 {
+  justify-content: center;
   display: flex;
-  flex-direction: row;
-  gap: 46px;
-  
-  }
-  
-  .susun {
+  align-items: center;
+  width: 100%;
+}
+
+.info1 p {
+  font-size: 2vw;
+  margin-right: 4%;
+  font-weight: 400;
+  color: #212121;
+  width: 50%;
+}
+
+.info1 img {
+  border-radius: 10px;
+  width: 50%;
+}
+
+.info2, .info3, .info4, .info5 {
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  padding-top: 9vw;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.info2 p, .info3 p, .info4 p, .info5 p {
+  font-weight: 400;
+  color: #212121;
+}
+
+.info2 p, .info4-text p {
+  text-align: right;
+  margin-left: 4vw;
+}
+
+.info3 p, .info5 p {
+  text-align: left;
+  margin-right: 4vw;
+}
+
+.between, .between4 {
+  white-space: nowrap;
+  margin-right: 2vw;
+  padding-bottom: 1vw;
+}
+
+.between2, .between5 {
+  margin-top: -0.4vw;
+  padding-left: 1.5vw;
+}
+
+.info2-text, .info3-text, .info4-text, .info5-text {
+  display: flex;
+  width: 60%;
+  font-size: 2vw;
+}
+
+.img-keraton2, .img-keraton3, .img-keraton4, .img-keraton5 {
+  border-radius: 10px;
+  width: 45%;
+  align-self: center;
+}
+
+.time {
+  height: 30vw;
+  align-self: baseline;
+}
+
+.time5 {
+  height: 53vw;
+  align-self: baseline;
+}
+
+.time1 {
+  height: 34vw;
+  align-self: baseline;
+}
+
+.logo {
+  font-size: 1.6vw;
+  font-weight: 400;
+  display: flex;
+  margin-left: 1vw;
+}
+
+.logo img {
+  height: 7vw;
+  margin-right: 1vw;
+}
+
+li a {
+  font-size: 1.4vw;
+}
+
+li {
+  margin-bottom: 0.6vw !important;
+}
+
+ul {
+  margin-top: 1vw;
+  line-height: 2vw;
+}
+
+h4 {
+  font-size: 2.3vw;
+  line-height: 2.8vw;
+}
+
+.footer-col {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 20vw;
+  margin-top: 4vw;
+}
+
+.footer-col-1 {
+  width: 16vw;
+  padding: 1.5vw;
+  box-sizing: border-box;
+}
+
+.footer-col-2,
+.footer-col-3 {
+  width: 12vw;
+  padding: 1.5vw;
+  box-sizing: border-box;
+}
+
+.footer-col-4 {
+  margin-left: 6vw;
+  padding: 1.5vw;
+  box-sizing: border-box;
+  width: 40vw;
+}
+
+.footer-col h3 {
+  margin-bottom: 20px;
+}
+
+.footer-col ul {
+  list-style: none;
+  padding: 0;
+}
+
+.footer-col li {
+  margin-bottom: 10px;
+}
+
+.footer-col a {
+  text-decoration: none;
+  color: #333;
+}
+
+.button {
+  margin-top: 2vw;
+  height: 5vw;
+  width: 100%;
+  padding-left: 1vw;
+  background-color: #123B32;
+  color: #fff;
+  text-align: left;
+  border: none;
+  cursor: pointer;
+  font-size: 1.5vw;
+  border-radius: 1vw;
+}
+
+.footer {
+  width: 90%;
+  margin: 0 auto;
+  padding-top: 10vw;
+}
+
+.adress {
+  width: 18vw;
+  font-size: 1.6vw;
+  font-weight: 400;
+  padding: 1.5vw;
+}
+
+.susun p {
+  font-size: 1.5vw;
+}
+.lower {
+  display: flex;
+  width: 100%;
+  padding-bottom: 4vw;
+}
+.inlower {
+  width: 50vw;
+}
+.inlower p{
+  margin-top: 10vw;
+  font-size: 1.3vw;
+  padding-left: 1.5vw;
+}
+.susun {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 46px;
-  padding-left: 795px;
-  }
-  </style>
+  gap: 2vw;
+  float: right;
+  width: fit-content;
+  padding-right: 4vw;
+}
+
+.foto1, .foto2, .foto3 {
+  height: 5vw;
+  width: 5vw;
+}
+
+.foto4 {
+  height: 7vw;
+  width: 7vw;
+  margin-top: -1vw;
+}
+.collab {
+  display: flex;
+  flex-direction: row;
+  gap: 4vw;
+  width: 40vw;
+  justify-content: center
+}
+</style>
