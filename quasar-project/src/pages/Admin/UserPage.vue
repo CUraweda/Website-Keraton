9773333ef03419cb5c6fc18e18ce03179973782b
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-06-06 11:25:18 +0700

    dsabhjhjas

---
+++ quasar-project/src/pages/Admin/UserPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import cookieHandler from "src/cookieHandler";
+import env from "stores/environment";
+
+export default {
+    data() {
+        return {
+            token: cookieHandler.getCookie(env.TOKEN_STORAGE_NAME),
+            userDatas: ref([]),
+            userRoleOptions: [
+                { label: "Super Admin", value: "SUPER_ADMIN" },
+                { label: "Admin", value: "ADMIN" },
+                { label: "Customer ", value: "CUSTOMER" },
+                { label: "Cashier", value: "CASHIER" },
+            ]
+        }
+    },
+    mounted(){
+        this.fetchData()
+    },
+    methods: {
+        async fetchData() {
+            try {
+                const response = await this.$api.get('user', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
+}
+</script>
